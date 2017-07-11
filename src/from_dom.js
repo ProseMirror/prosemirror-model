bde28a1a4278fd4ddf57276fe78b9705cc1cceaf
Marijn Haverbeke <marijn@haverbeke.nl>
2017-07-11 11:36:50 +0200

    Refine heuristic for when to ignore whitespace-only text nodes
    FIX: Fix issue where whitespace at node boundaries was sometimes dropped
    during content parsing.
    
    Issue prosemirror/prosemirror#671

--- src/from_dom.js
+++ src/from_dom.js
@@ -358,7 +358,7 @@ class ParseContext {
   addTextNode(dom) {
     let value = dom.nodeValue
     let top = this.top
-    if ((top.type && top.type.inlineContent) || /\S/.test(value)) {
+    if ((top.type ? top.type.inlineContent : top.content.length && top.content[0].isInline) || /\S/.test(value)) {
       if (!(top.options & OPT_PRESERVE_WS)) {
         value = value.replace(/\s+/g, " ")
         // If this starts with whitespace, and there is either no node
