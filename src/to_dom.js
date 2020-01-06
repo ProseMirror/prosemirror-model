66df62561e942011d3686a6f61a195ff2f4125de
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-01-06 18:34:19 +0900

    Add support for rendering namespaced elements and attributes
    FEATURE: Element and attribute names in
    [`DOMOutputSpec`](##model.DOMOutputSpec) structures can now contain
    namespaces.

--- src/to_dom.js
+++ src/to_dom.js
@@ -4,12 +4,12 @@
 // itself, or an array.
 //
 // An array describes a DOM element. The first value in the array
-// should be a string—the name of the DOM element. If the second
-// element is plain object, it is interpreted as a set of attributes
-// for the element. Any elements after that (including the 2nd if it's
-// not an attribute object) are interpreted as children of the DOM
-// elements, and must either be valid `DOMOutputSpec` values, or the
-// number zero.
+// should be a string—the name of the DOM element, optionally prefixed
+// by a namespace URL and a space. If the second element is plain
+// object, it is interpreted as a set of attributes for the element.
+// Any elements after that (including the 2nd if it's not an attribute
+// object) are interpreted as children of the DOM elements, and must
+// either be valid `DOMOutputSpec` values, or the number zero.
 //
 // The number zero (pronounced “hole”) is used to indicate the place
 // where a node's child nodes should be inserted. If it occurs in an
@@ -116,17 +116,28 @@ export class DOMSerializer {
   // Render an [output spec](#model.DOMOutputSpec) to a DOM node. If
   // the spec has a hole (zero) in it, `contentDOM` will point at the
   // node with the hole.
-  static renderSpec(doc, structure) {
+  static renderSpec(doc, structure, parentXmlns = null) {
     if (typeof structure == "string")
       return {dom: doc.createTextNode(structure)}
     if (structure.nodeType != null)
       return {dom: structure}
-    let dom = doc.createElement(structure[0]), contentDOM = null
+    let contentDOM = null, xmlns = parentXmlns, tag
+    if (structure[0].indexOf(" ") > -1) [xmlns, tag] = structure[0].split(" ", 2)
+    else tag = structure[0]
+    let dom = xmlns ? doc.createElementNS(xmlns, tag) : doc.createElement(tag)
     let attrs = structure[1], start = 1
     if (attrs && typeof attrs == "object" && attrs.nodeType == null && !Array.isArray(attrs)) {
       start = 2
       for (let name in attrs) {
-        if (attrs[name] != null) dom.setAttribute(name, attrs[name])
+        if (attrs[name] != null) {
+          let [xmlnsAttr, nameWithoutXmlns] = name.split(' ', 2)
+          if (nameWithoutXmlns) {
+            dom.setAttributeNS(xmlnsAttr, nameWithoutXmlns, attrs[name])
+          } else {
+            dom.setAttribute(name, attrs[name])
+          }
+
+        }
       }
     }
     for (let i = start; i < structure.length; i++) {
@@ -136,7 +147,7 @@ export class DOMSerializer {
           throw new RangeError("Content hole must be the only child of its parent node")
         return {dom, contentDOM: dom}
       } else {
-        let {dom: inner, contentDOM: innerContent} = DOMSerializer.renderSpec(doc, child)
+        let {dom: inner, contentDOM: innerContent} = DOMSerializer.renderSpec(doc, child, xmlns)
         dom.appendChild(inner)
         if (innerContent) {
           if (contentDOM) throw new RangeError("Multiple content holes")
