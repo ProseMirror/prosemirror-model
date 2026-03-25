042fe20a74e7d4dd9b664e9c02040fd411aedf31
Marijn Haverbeke <marijn@haverbeke.berlin>
2026-03-25 07:33:27 +0100

    Disallow strings at the top level of DOM output specs
    FIX: Properly reflect the fact that nodes must not serialize to just text nodes in
    the `DOMOutputSpec` type.
    
    Closes https://github.com/ProseMirror/prosemirror/issues/1566

--- src/to_dom.ts
+++ src/to_dom.ts
@@ -4,23 +4,23 @@ import {Schema, NodeType, MarkType} from "./schema"
 import {Mark} from "./mark"
 import {DOMNode} from "./dom"
 
-/// A description of a DOM structure. Can be either a string, which is
-/// interpreted as a text node, a DOM node, which is interpreted as
-/// itself, a `{dom, contentDOM}` object, or an array.
+/// A description of a DOM structure. Can be either a DOM node, a
+/// `{dom, contentDOM}` object, or an array.
 ///
 /// An array describes a DOM element. The first value in the array
-/// should be a string—the name of the DOM element, optionally prefixed
-/// by a namespace URL and a space. If the second element is plain
-/// object, it is interpreted as a set of attributes for the element.
-/// Any elements after that (including the 2nd if it's not an attribute
-/// object) are interpreted as children of the DOM elements, and must
-/// either be valid `DOMOutputSpec` values, or the number zero.
+/// should be a string—the name of the DOM element, optionally
+/// prefixed by a namespace URL and a space. If the second element is
+/// plain object, it is interpreted as a set of attributes for the
+/// element. Any elements after that (including the 2nd if it's not an
+/// attribute object) are interpreted as children of the DOM elements,
+/// and must either be valid `DOMOutputSpec` values, strings (for text
+/// nodes), or the number zero.
 ///
 /// The number zero (pronounced “hole”) is used to indicate the place
 /// where a node's child nodes should be inserted. If it occurs in an
 /// output spec, it should be the only child element in its parent
 /// node.
-export type DOMOutputSpec = string | DOMNode | {dom: DOMNode, contentDOM?: HTMLElement} | readonly [string, ...any[]]
+export type DOMOutputSpec = DOMNode | {dom: DOMNode, contentDOM?: HTMLElement} | readonly [string, ...any[]]
 
 /// A DOM serializer knows how to convert ProseMirror nodes and
 /// marks of various types to DOM nodes.
@@ -186,7 +186,7 @@ function suspiciousAttributesInner(attrs: {[name: string]: any}): readonly any[]
   return result
 }
 
-function renderSpec(doc: Document, structure: DOMOutputSpec, xmlNS: string | null,
+function renderSpec(doc: Document, structure: DOMOutputSpec | string, xmlNS: string | null,
                     blockArraysIn?: {[name: string]: any}): {
   dom: DOMNode,
   contentDOM?: HTMLElement
@@ -220,7 +220,7 @@ function renderSpec(doc: Document, structure: DOMOutputSpec, xmlNS: string | nul
     }
   }
   for (let i = start; i < (structure as readonly any[]).length; i++) {
-    let child = (structure as any)[i] as DOMOutputSpec | 0
+    let child = (structure as any)[i] as DOMOutputSpec | string | 0
     if (child === 0) {
       if (i < (structure as readonly any[]).length - 1 || i > start)
         throw new RangeError("Content hole must be the only child of its parent node")
